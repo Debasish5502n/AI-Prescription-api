077df5df0047625f86730169a3f9a00e4e0db806
utkallearninghub <utkallearninghub@gmail.com>
2026-02-22 19:24:53 +0530

    remove secret files

--- server.js
+++ server.js
@@ -175,6 +175,8 @@ Do not explain.
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
